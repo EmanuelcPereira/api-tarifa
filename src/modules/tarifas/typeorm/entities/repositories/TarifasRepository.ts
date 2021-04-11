74064d3194f681787eba6c9ae30f91d6282eddaa
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-04-11 11:11:05 -0300

    feat: created tarifa repository

---
+++ src/modules/tarifas/typeorm/entities/repositories/TarifasRepository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Tarifa from '../Tarifa';
+
+@EntityRepository(Tarifa)
+export default class TarifasRepository extends Repository<Tarifa> {
+  public async findByDDD(
+    origem: string,
+    destino: string,
+  ): Promise<Tarifa | undefined> {
+    const custoTarifa = this.findOne({ origem, destino });
+
+    return custoTarifa;
+  }
+}
